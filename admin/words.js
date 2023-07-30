bde57042fca9884b5d5c28f3036f7abb8e760631
XSDiVer <Tachyonen@quantentunnel.de>
2023-07-30 10:56:06 +0200

    translate update

--- admin/words.js
+++ admin/words.js
@@ -8,112 +8,23 @@
 'use strict';
 
 systemDictionary = {
-	'time_switch_clock adapter settings': {
-		'en': 'Adapter settings for time_switch_clock',
-		'de': 'Adaptereinstellungen für time_switch_clock',
-		'ru': 'Настройки адаптера для time_switch_clock',
-		'pt': 'Configurações do adaptador para time_switch_clock',
-		'nl': 'Adapterinstellingen voor time_switch_clock',
-		'fr': "Paramètres d'adaptateur pour time_switch_clock",
-		'it': "Impostazioni dell'adattatore per time_switch_clock",
-		'es': 'Ajustes del adaptador para time_switch_clock',
-		'pl': 'Ustawienia adaptera dla time_switch_clock',
-		'zh-cn': 'time_switch_clock的适配器设置'
-	},
-	'Option 1': {
-		'en': 'Option 1',
-		'de': 'Option 1',
-		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'
-	},
-	'Option 2': {
-		'en': 'Option 2',
-		'de': 'Option 2',
-		'ru': 'вариант 2',
-		'pt': 'opção 2',
-		'nl': 'Optie 2',
-		'fr': 'Option 2',
-		'it': 'opzione 2',
-		'es': 'opcion 2',
-		'pl': 'Opcja 2',
-		'zh-cn': '选项2'
-	},
-	'myColor': {
-		'en': 'myColor',
-		'de': 'meineColor',
-		'ru': 'Мой цвет',
-		'pt': 'minhaCor',
-		'nl': 'mijnKleur',
-		'fr': 'maCouleur',
-		'it': 'mioColore',
-		'es': 'miColor',
-		'pl': 'mójKolor',
-		'zh-cn': '我的颜色'
-	},
-	'myColor_tooltip': {
-		'en': 'Description of\nmyColor',
-		'de': 'Beschreibung von\nmyColor',
-		'ru': 'Описание\nmyColor',
-		'pt': 'Descrição de\nmyColor',
-		'nl': 'Beschrijving van\nmyColor',
-		'fr': 'Description de\nmyColor',
-		'it': 'Descrizione di\nmyColor',
-		'es': 'Descripción de\nmyColor',
-		'pl': 'Opis\nmyColor',
-		'zh-cn': '\nmyColor的描述'
-	},
-	'htmlText': {
-		'en': 'htmlText',
-		'de': 'htmlText',
-		'ru': 'htmlText',
-		'pt': 'htmlText',
-		'nl': 'htmlText',
-		'fr': 'htmlText',
-		'it': 'htmlText',
-		'es': 'htmlText',
-		'pl': 'htmlText',
-		'zh-cn': 'htmlText'
-	},
-	'group_extraMyset': {
-		'en': 'extraMyset',
-		'de': 'extraMyset',
-		'ru': 'extraMyset',
-		'pt': 'extraMyset',
-		'nl': 'extraMyset',
-		'fr': 'extraMyset',
-		'it': 'extraMyset',
-		'es': 'extraMyset',
-		'pl': 'extraMyset',
-		'zh-cn': 'extraMyset'
-	},
-	'extraAttr': {
-		'en': 'extraAttr',
-		'de': 'extraAttr',
-		'ru': 'extraAttr',
-		'pt': 'extraAttr',
-		'nl': 'extraAttr',
-		'fr': 'extraAttr',
-		'it': 'extraAttr',
-		'es': 'extraAttr',
-		'pl': 'extraAttr',
-		'zh-cn': 'extraAttr'
-	},
-	'Instance': {
-		'en': 'Instance',
-		'de': 'Instanz',
-		'ru': 'Инстанция',
-		'pt': 'Instância',
-		'nl': 'Instantie',
-		'fr': 'Instance',
-		'it': 'Esempio',
-		'es': 'Instancia',
-		'pl': 'Instancja',
-		'zh-cn': '例'
-	}
+    "time_switch_clock adapter settings": {           "en": "Adapter settings for time_switch_clock",           "de": "Adaptereinstellungen für time_switch_clock",       "ru": "Настройки адаптера для time_switch_clock",         "pt": "Configurações do adaptador para time_switch_clock", "nl": "Adapterinstellingen voor time_switch_clock",       "fr": "Paramètres d'adaptateur pour time_switch_clock",   "it": "Impostazioni dell'adattatore per time_switch_clock", "es": "Ajustes del adaptador para time_switch_clock",     "pl": "Ustawienia adaptera dla time_switch_clock",        "zh-cn": "time_switch_clock的适配器设置"},
+    "Option 1": {                                     "en": "Option 1",                                         "de": "Option 1",                                         "ru": "Опция 1",                                          "pt": "Opção 1",                                          "nl": "Optie 1",                                          "fr": "Option 1",                                         "it": "opzione 1",                                        "es": "Opción 1",                                         "pl": "opcja 1",                                          "zh-cn": "选项1"},
+    "Option 2": {                                     "en": "Option 2",                                         "de": "Option 2",                                         "ru": "вариант 2",                                        "pt": "opção 2",                                          "nl": "Optie 2",                                          "fr": "Option 2",                                         "it": "opzione 2",                                        "es": "opcion 2",                                         "pl": "Opcja 2",                                          "zh-cn": "选项2"},
+    "myColor": {                                      "en": "myColor",                                          "de": "meineColor",                                       "ru": "Мой цвет",                                         "pt": "minhaCor",                                         "nl": "mijnKleur",                                        "fr": "maCouleur",                                        "it": "mioColore",                                        "es": "miColor",                                          "pl": "mójKolor",                                         "zh-cn": "我的颜色"},
+    "myColor_tooltip": {                              "en": "Description of
+myColor",                           "de": "Beschreibung von
+myColor",                         "ru": "Описание
+myColor",                                 "pt": "Descrição de
+myColor",                             "nl": "Beschrijving van
+myColor",                         "fr": "Description de
+myColor",                           "it": "Descrizione di
+myColor",                           "es": "Descripción de
+myColor",                           "pl": "Opis
+myColor",                                     "zh-cn": "
+myColor的描述"},
+    "htmlText": {                                     "en": "htmlText",                                         "de": "htmlText",                                         "ru": "htmlText",                                         "pt": "htmlText",                                         "nl": "htmlText",                                         "fr": "htmlText",                                         "it": "htmlText",                                         "es": "htmlText",                                         "pl": "htmlText",                                         "zh-cn": "htmlText"},
+    "group_extraMyset": {                             "en": "extraMyset",                                       "de": "extraMyset",                                       "ru": "extraMyset",                                       "pt": "extraMyset",                                       "nl": "extraMyset",                                       "fr": "extraMyset",                                       "it": "extraMyset",                                       "es": "extraMyset",                                       "pl": "extraMyset",                                       "zh-cn": "extraMyset"},
+    "extraAttr": {                                    "en": "extraAttr",                                        "de": "extraAttr",                                        "ru": "extraAttr",                                        "pt": "extraAttr",                                        "nl": "extraAttr",                                        "fr": "extraAttr",                                        "it": "extraAttr",                                        "es": "extraAttr",                                        "pl": "extraAttr",                                        "zh-cn": "extraAttr"},
+    "Instance": {                                     "en": "Instance",                                         "de": "Instanz",                                          "ru": "Инстанция",                                        "pt": "Instância",                                        "nl": "Instantie",                                        "fr": "Instance",                                         "it": "Esempio",                                          "es": "Instancia",                                        "pl": "Instancja",                                        "zh-cn": "例"},
 };
